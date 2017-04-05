004cb2a12e5613f040a47a8a20aa52645e570366
Rigdon Ware <rigdon.ware@gmail.com>
2017-04-05 18:16:51 +0000

    Created a new database file under models.py

--- pollsApp/models.py
+++ pollsApp/models.py
@@ -3,3 +3,12 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length = 260)
+    pub_data = models.DateTimeField('date published')
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete = models.CASCADE)
+    choice_text = models.CharField(max_length = 260)
+    votes = models.IntegerField(default = 0)
