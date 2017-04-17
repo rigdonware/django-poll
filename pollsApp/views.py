27afc32d806fa5f12ce1f197e0df37b8d969e453
Rigdon Ware <rigdon.ware@gmail.com>
2017-04-17 05:07:36 +0000

    fixed admin.py to include the Choices to be edited under admin page

--- pollsApp/views.py
+++ pollsApp/views.py
@@ -22,22 +22,6 @@ class ResultsView(generic.DetailView):
     model = Question
     template_name = 'pollsApp/results.html'
 
-# def index(request):
-#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-#     context = {'latest_question_list': latest_question_list,}
-#     return render(request, 'pollsApp/index.html', context)
-    
-# def detail(request, question_id):
-#     try:
-#         question = Question.objects.get(pk = question_id)
-#     except Question.DoesNotExist:
-#         raise Http404("Question does not exist")
-#     return render(request, 'pollsApp/detail.html', {'question': question})
-    
-# def results(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     return render(request, 'pollsApp/results.html', {'question': question})
-    
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
